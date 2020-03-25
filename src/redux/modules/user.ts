3068494a5f417d074dcfd45305731bf398c5b4b2
Masaaki Kodama <tc003ssr@gmail.com>
2020-03-26 01:08:02 +0900

    feat: User typeを追加

--- src/redux/modules/user.ts
+++ src/redux/modules/user.ts
@@ -1,4 +1,13 @@
-export async function getUser() {
+export type User = {
+  id: string;
+  name: string;
+};
+
+export async function getUser(): Promise<User> {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   console.log('Get Usre !!');
+  return {
+    id: 'NFfdho18EJhi8fnkHFdsFDSJh0',
+    name: 'ハードコーディングユーザ',
+  };
 }
